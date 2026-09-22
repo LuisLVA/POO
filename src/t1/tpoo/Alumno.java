4f83a29b03ed6811b3055ad30e0637a1072f682a
yu <yu@gmail.com>
2026-09-21 19:57:13 -0500

    Aaaa

---
+++ src/t1/tpoo/Alumno.java
@@ -0,0 +1,46 @@
+package t1.tpoo;
+
+public class Alumno 
+{
+  private String nombre;  
+  private String TipoDeDocumento;
+  private String NivelSE;
+  private String TipoDeBeca;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoDeDocumento() {
+        return TipoDeDocumento;
+    }
+
+    public void setTipoDeDocumento(String TipoDeDocumento) {
+        this.TipoDeDocumento = TipoDeDocumento;
+    }
+
+    public String getNivelSE() {
+        return NivelSE;
+    }
+
+    public void setNivelSE(String NivelSE) {
+        this.NivelSE = NivelSE;
+    }
+
+    public String getTipoDeBeca() {
+        return TipoDeBeca;
+    }
+
+    public void setTipoDeBeca(String TipoDeBeca) {
+        this.TipoDeBeca = TipoDeBeca;
+    }
+  
+  
+  
+ 
+  
+}
